fafe823c44792568866097745fb103eeae71c522
Kyryl <kyryl.zp@gmail.com>
2022-09-30 23:40:38 +0300

    Create Friends App

---
+++ js/getData.js
@@ -0,0 +1,7 @@
+let URL_API = 'https://randomuser.me/api/?results=50&nat=us,gb,fr,ua&inc=gender,name,email,dob,phone,picture';
+
+const getData = () => fetch(URL_API)
+    .then(response => response.json())
+    .catch((err) => {console.error(err)});
+
+export default getData
