ac7409f34b412d0a5bd3f9a9c2c67380513827a2
Kyryl <kyryl.zp@gmail.com>
2022-11-06 22:29:53 +0200

    correction bags

--- js/index.js
+++ js/index.js
@@ -1,21 +1,18 @@
-import getUsers from './getData.js';
+import getUsers from './getUsers.js';
 import removeMenu from './mobile.js';
 import preparePhoneNumber from './preparePhoneNumber.js';
 
-const app = document.querySelector('.main');
+const locationCardUsers = document.querySelector('.main');
 const functionForm = document.querySelector('#function_menu');
-const countFriends = document.querySelector('.count_friends');
 const resetBtn = document.querySelector('.reset_btn');
-
 let response = [];
-let users = [];
 
 const createdCardUser = ({ name, gender, email, phone, picture, dob }) => {
     const card = document.createElement('article');
     card.classList.add('card');
 
-    let phones = Number(phone.replace(/\D+/g,''));
-    let result = preparePhoneNumber(phones);
+    const formationPhone = Number(phone.replace(/\D+/g,''));
+    const phoneUser = preparePhoneNumber(formationPhone);
 
     if(gender === 'female') {
         card.classList.add('female');
@@ -27,7 +24,7 @@ const createdCardUser = ({ name, gender, email, phone, picture, dob }) => {
             <div class="card_block_img"><img class="card_img" src="${picture.large}" alt="${name.first}"></div>
             <p class="card_age">I am ${dob.age} years old</p>
             <p class="card_email">${email}</p>
-            <p class="card_phone"><a href="tel:${phones}">${result}</a></p>
+            <p class="card_phone"><a href="tel:${formationPhone}">${phoneUser}</a></p>
         </div>
         <p class="card_human" id="card_human">${gender}</p>       
     `);
@@ -36,7 +33,7 @@ const createdCardUser = ({ name, gender, email, phone, picture, dob }) => {
 };
 
 const sortUsers = (users) => {
-    const byField = (a, b) => {
+    const sortByField = (a, b) => {
         return a > b ? 1 : -1;
     };
 
@@ -47,24 +44,22 @@ const sortUsers = (users) => {
     }
 
     if (selectSortsUser === 'age_up') {
-        return users.sort((a, b) => byField(a.dob.age, b.dob.age));
+        return users.sort((a, b) => sortByField(a.dob.age, b.dob.age));
     }
     if (selectSortsUser === 'age_down') {
-        return users.sort((a, b) => byField(b.dob.age, a.dob.age));
+        return users.sort((a, b) => sortByField(b.dob.age, a.dob.age));
     }
     if (selectSortsUser === 'name_fromA') {
-        return users.sort((a, b) => byField(a.name.first, b.name.first));
+        return users.sort((a, b) => sortByField(a.name.first, b.name.first));
     }
     if (selectSortsUser === 'name_fromZ') {
-        return users.sort((a, b) => byField(b.name.first, a.name.first));
+        return users.sort((a, b) => sortByField(b.name.first, a.name.first));
     }
 };
 
 const filterByGender = (users) => {
     const gendersValue = functionForm.gender.value;
-    if (gendersValue === 'all') {
-        return users;
-    }
+    if (gendersValue === 'all') return users;
     return users.filter(user => user.gender === gendersValue);
 };
 
@@ -88,17 +83,12 @@ const renderUsers = () => {
 };
 
 const renderCards = (users) => {
-    const countAllUsers = users.map(e => e.name.first).length;
-    countFriends.textContent = `${countAllUsers}`;
-
-    app.textContent = '';
+    locationCardUsers.textContent = '';
     const cardUsers = users.map(createdCardUser);
-    app.append(...cardUsers);
+    locationCardUsers.append(...cardUsers);
 };
 
-functionForm.addEventListener('input', () => {
-    renderUsers();
-});
+functionForm.addEventListener('input', renderUsers);
 
 resetBtn.addEventListener('click', () => {
     renderCards([...response.results]);
@@ -106,7 +96,6 @@ resetBtn.addEventListener('click', () => {
 
 const init = async () => {
     response = await getUsers();
-    users = [...response.results];
     renderUsers();
     removeMenu();
 };
